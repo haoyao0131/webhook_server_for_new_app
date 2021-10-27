27c7a9106ccc2a1de040fd69cb31043fd2a11f32
haoyao0131 <40881644+haoyao0131@users.noreply.github.com>
2021-10-27 12:37:10 -0700

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -10,7 +10,7 @@ SEND_API_URL = 'https://graph.facebook.com/v5.0/me/messages?access_token=%s'\
   %PAGE_ACCESS_TOKEN
 
 HEADERS = {'content-type': 'application/json'}
-IG_ACC_TO_REPLY = '90010625844858'
+IG_ACC_TO_REPLY = '17841447086190741'
       
       
 def send_message(body):
